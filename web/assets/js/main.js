8b8933efd5e7ea8cef7c90382345ffec156e2909
Gustav Moesmand <gustavmoesmand@gmail.com>
2021-12-03 21:11:32 +0100

    added swiper

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -29,3 +29,22 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== HOME SWIPER ====================*/
+let homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: "true",
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
